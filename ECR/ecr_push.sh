dfb9f1a2079f070b4d8060836aa7f64885baef9a
bharath <bhrth.dsra1@gmail.com>
2019-09-04 15:11:40 +0800

    push docker images ecr registry

---
+++ ECR/ecr_push.sh
@@ -0,0 +1,9 @@
+Bharaths-MacBook-Pro:external bharathdasaraju$ docker tag nginx:latest 172586632398.dkr.ecr.ap-southeast-1.amazonaws.com/hanumans:0.0.1
+
+Bharaths-MacBook-Pro:external bharathdasaraju$ docker push 172586632398.dkr.ecr.ap-southeast-1.amazonaws.com/hanumans:0.0.1
+The push refers to repository [172586632398.dkr.ecr.ap-southeast-1.amazonaws.com/hanumans]
+12fdf55172df: Pushed
+002a63507c1c: Pushed
+1c95c77433e8: Pushed
+0.0.1: digest: sha256:099019968725f0fc12c4b69b289a347ae74cc56da0f0ef56e8eb8e0134fc7911 size: 948
+Bharaths-MacBook-Pro:external bharathdasaraju$
