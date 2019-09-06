3c3224f336398c5e43f66ac910e454d076f49794
Bharath <bharath@hooq.tv>
2019-09-06 16:31:48 +0800

    fish shell for life

---
+++ fish_shell/commands.sh
@@ -0,0 +1,14 @@
+brew install fish
+curl -L https://get.oh-my.fish | fish
+omf install bobthefish
+omf theme bobthefish
+
+pip install --user powerline-status
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts/
+./install.sh
+brew install coreutils
+brew install inetutils
+pip3 install pygments
+brew install tmux
+brew install ranger
